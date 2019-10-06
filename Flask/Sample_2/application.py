f88dbb0ee6a9248b78f0d6be501e5c1c1fc12654
Seiya Kobayashi <seiyak@bu.edu>
2019-10-05 22:52:46 -0400

    Update Flask sample app.

--- Flask/Sample_2/application.py
+++ Flask/Sample_2/application.py
@@ -1,6 +1,5 @@
-from flask import Flask, render_template, request, redirect
-
-app = Flask(__name__)
+from flask import render_template, request, redirect
+from init_db import app, get_db, insert_db, query_db
 
 @app.route("/")
 def index():
@@ -18,7 +17,17 @@ def register():
         email = request.form.get("email")
         password = request.form.get("password")
         gender = request.form.get("gender")
-        return render_template("success.html", name=name, email=email, password=password, gender=gender)
+
+        insert_db("INSERT INTO user (name, email, password, gender) \
+                   VALUES(?, ?, ?, ?)", (name, email, password, gender))
+        user = query_db("SELECT * FROM user WHERE email = ?", (email,), True)
+
+        return render_template("success.html", user=user)
+
+@app.route("/view")
+def view():
+    users = query_db("SELECT * FROM user")
+    return render_template("view.html", users=users)
 
 if __name__ == '__main__':
     app.run()
